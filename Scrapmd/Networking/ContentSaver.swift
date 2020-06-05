53ede1ba97b5031e2944eccc0ebddc2cbb2ca1ca
Atsushi Nagase <a@ngs.io>
2020-06-05 12:12:17 +0900

    Save image

--- Scrapmd/Networking/ContentSaver.swift
+++ Scrapmd/Networking/ContentSaver.swift
@@ -20,6 +20,10 @@ struct ContentSaver {
             let task = URLSession.shared.downloadTask(with: url) { (url, _, err) in
                 do {
                     let img = ImageFile(path: Path(url: url!)!)
+                    if self.destination.exists {
+                        try self.destination.delete()
+                    }
+                    try self.destination.path.parent.createDirectory(withIntermediateDirectories: true)
                     try img.move(to: self.destination.path)
                     completionHandler(self, nil)
                 } catch {
@@ -42,7 +46,7 @@ struct ContentSaver {
     typealias ProgressHandler = (ProgressInfo, Int, Int) -> Void
     typealias CompletionHandler = (Error?) -> Void
 
-    func download(to: Path, progress: ProgressHandler, completionHandler: @escaping CompletionHandler) {
+    func download(to: Path, progress: @escaping ProgressHandler, completionHandler: @escaping CompletionHandler) {
 
         do {
             try to.createDirectory(withIntermediateDirectories: true)
@@ -52,6 +56,7 @@ struct ContentSaver {
             return
         }
         var queues: [DownloadQueue] = []
+        var total = 0
         for (sum, url) in result.images {
             let queue = ProgressInfo(url: URL(string: url)!, destination: ImageFile(path: to + sum)).queue { (info, err) in
                 if let err = err {
@@ -60,13 +65,17 @@ struct ContentSaver {
                 }
                 if queues.isEmpty {
                     completionHandler(nil)
+                    return
                 }
                 let q = queues.removeFirst()
+                progress(q.info, total - queues.count - 1, total)
                 q.resume()
             }
             queues.append(queue)
         }
+        total = queues.count
         let q = queues.removeFirst()
+        progress(q.info, 0, total)
         q.resume()
     }
 }
