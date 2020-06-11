ac556b9da7169e2fbde001460f61b6c35996ec75
Atsushi Nagase <a@ngs.io>
2020-06-11 19:53:54 +0900

    Integrate Firebase

--- Scrapmd/AppDelegate.swift
+++ Scrapmd/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
