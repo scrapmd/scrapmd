042e9c1c5c628902eab1883056f0195a82f34f0f
Atsushi Nagase <a@ngs.io>
2020-06-11 15:13:15 +0900

    Add Copyright to Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,3 +1,11 @@
+//
+//  Dangerfile.swift
+//  Scrapmd
+//
+//  Created by Atsushi Nagase on 2020/06/05.
+//  Copyright © 2020 LittleApps Inc. All rights reserved.
+//
+
 import Danger
 import Foundation
 
